dde95818f9002446945889054116c0f5c6490075
zjensen262 <31410065+zjensen262@users.noreply.github.com>
2020-05-08 13:57:55 -0400

    test

--- tableextractor/table_extractor.py
+++ tableextractor/table_extractor.py
@@ -821,7 +821,7 @@ class TableExtractor(object):
         for word, label in zip(words, labels):
             if str(word) in self.embeddings:
                 print(word)
-                print(type(self.embeddings[str(word)]))
+                print(self.embeddings[str(word)].shape)
                 emb_vector.append(self.embeddings[str(word)])
                 label_vector.append(label)
             else:
